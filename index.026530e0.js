c7e1937be8111288446535f1b496105e8e644a86
Hanna <hmatviienko@gmail.com>
2024-04-30 13:12:12 +0300

    make build

---
+++ index.026530e0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('a[href="#aside"]'),c=document.querySelector(".aside__cover--top--cover--close"),o=document.body;e.addEventListener("click",()=>{o.classList.add("no-scroll")}),c.addEventListener("click",()=>{o.classList.remove("no-scroll")});
+//# sourceMappingURL=index.026530e0.js.map
